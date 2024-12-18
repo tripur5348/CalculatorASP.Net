d37b271e17a243f7c0fe94f18c0c9cddfb9d36b9
tripur5348 <tripurmuley5348@gmail.com>
2024-12-18 19:30:45 +0530

    Second commit

--- MVCCalculator/obj/Debug/net8.0/MVCCalculator.AssemblyInfo.cs
+++ MVCCalculator/obj/Debug/net8.0/MVCCalculator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCCalculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+017a08332769c3c484235372b221f1ed80a87370")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCCalculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCCalculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
